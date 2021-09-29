9fe8f3db73b08b4bcfc61c40d526e826b318bf48
zada8 <qqaz0609@naver.com>
2021-09-29 17:06:08 +0900

    commit

---
+++ app/src/main/java/com/android/hearwego/RouteChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.android.hearwego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RouteChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.route_choice);
+    }
+}
