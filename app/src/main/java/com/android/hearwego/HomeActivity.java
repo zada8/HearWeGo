14d51ea519dd913e01a0240a7b7d670ae43888fa
siyunkr <siyunkr@gmail.com>
2021-09-29 23:24:29 +0900

    activities - imagebutton 객체 참조 선언

--- app/src/main/java/com/android/hearwego/HomeActivity.java
+++ app/src/main/java/com/android/hearwego/HomeActivity.java
@@ -10,12 +10,12 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting);
+        setContentView(R.layout.home);
 
-        ImageButton button_navi = findViewById(R.id.help); //메뉴1: 길 안내 이미지 버튼 객체 참조
-        ImageButton button_surround = findViewById(R.id.logout); //메뉴2: 주변시설 이미지 버튼 객체 참조
-        ImageButton button_bookmark = findViewById(R.id.withdraw); //메뉴3: 즐겨찾기 이미지 버튼 객체 참조
-        ImageButton button_setting = findViewById(R.id.previous); //메뉴4: 설정 이미지 버튼 객체 참조
+        ImageButton button_navi = findViewById(R.id.menu_navi); //메뉴1: 길 안내 이미지 버튼 객체 참조
+        ImageButton button_surround = findViewById(R.id.menu_surround); //메뉴2: 주변시설 이미지 버튼 객체 참조
+        ImageButton button_bookmark = findViewById(R.id.menu_bookmark); //메뉴3: 즐겨찾기 이미지 버튼 객체 참조
+        ImageButton button_setting = findViewById(R.id.menu_setting); //메뉴4: 설정 이미지 버튼 객체 참조
     }
 
 }
