6a1ee1b05a38590db7f379d3f330d2e662077451
zada8 <qqaz0609@naver.com>
2021-10-01 17:20:40 +0900

    북마크 수정 액티비티

--- app/src/main/java/com/android/hearwego/BookmarkEdit.java
+++ app/src/main/java/com/android/hearwego/BookmarkEdit.java
@@ -1,13 +1,46 @@
 package com.android.hearwego;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class BookmarkEdit extends AppCompatActivity {
+    private View decorView; //full screen 객체 선언
+    private int	uiOption; //full screen 객체 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmark_edit);
+
+        ActionBar actionBar = getSupportActionBar(); //액션바(패키지명) 숨김처리
+        actionBar.hide();
+
+        /*전체 화면 모드 -> 소프트 키 없앰*/
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
+            uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+            uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
+            uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility( uiOption );
+
+        Button button_delete_keyword = findViewById(R.id.delete_keyword);
+        Button button_delete_bookmark = findViewById(R.id.delete_bookmark);
+        Button button_set_destination = findViewById(R.id.set_destination);
+
+        ImageButton button_previous = findViewById(R.id.previous); //이전 이미지 버튼 객체 참조
+        ImageButton button_home = findViewById(R.id.home); // 홈 이미지 버튼 객체 참조
+
+
+
     }
 }
