f649a77e16a5d5b4dfe3564744b287f5373cc78b
siyunkr <siyunkr@gmail.com>
2021-11-02 20:06:48 +0900

    공공기관, 지하철역, 약국, 화장실 액티비티 완성

--- app/src/main/java/com/android/hearwego/SurroundingActivity.java
+++ app/src/main/java/com/android/hearwego/SurroundingActivity.java
@@ -36,18 +36,58 @@ public class SurroundingActivity extends AppCompatActivity {
 
         ImageButton button_facility = findViewById(R.id.menu_facility); //공공시설 이미지 버튼 객체 참조
         ImageButton button_subway = findViewById(R.id.menu_subway); //지하철 이미지 버튼 객체 참조
-        ImageButton button_toilet = findViewById(R.id.menu_toilet); //화장실 이미지 버튼 객체 참조
+        ImageButton button_restroom = findViewById(R.id.menu_toilet); //화장실 이미지 버튼 객체 참조
         ImageButton button_hospital = findViewById(R.id.menu_hospital); //병원 이미지 버튼 객체 참조
         ImageButton button_pharmacy = findViewById(R.id.menu_pharmacy); //약국 이미지 버튼 객체 참조
 
         ImageButton button_previous = findViewById(R.id.previous); //이전 이미지 버튼 객체 참조
         ImageButton button_home = findViewById(R.id.home); // 홈 이미지 버튼 객체 참조
 
+        //공공시설 버튼 누를 시 화면 전환
+        button_facility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SurroundingActivity.this, FacilityActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //지하철역 버튼 누를 시 화면 전환
+        button_subway.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SurroundingActivity.this, SubwayActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //화장실 버튼 누를 시 화면 전환
+        button_restroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SurroundingActivity.this, RestroomActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         //병원 버튼 누를 시 화면 전환
         button_hospital.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SurroundingActivity.this, HospitalActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //약국 버튼 누를 시 화면 전환
+        button_pharmacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SurroundingActivity.this, PharmacyActivity.class);
                 startActivity(intent);
                 finish();
             }
