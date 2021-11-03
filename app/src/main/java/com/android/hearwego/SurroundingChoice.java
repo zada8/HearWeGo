f158bc301a6e549544760bf507f9448f544802b5
siyunkr <siyunkr@gmail.com>
2021-11-03 13:06:51 +0900

    imagebutton 선언 수정

--- app/src/main/java/com/android/hearwego/SurroundingChoice.java
+++ app/src/main/java/com/android/hearwego/SurroundingChoice.java
@@ -44,8 +44,8 @@ public class SurroundingChoice extends AppCompatActivity {
         Button button_add_bookmark = findViewById(R.id.add_bookmark);
         Button button_set_destination = findViewById(R.id.set_destination);
 
-        ImageButton button_previous = findViewById(R.id.previous); //이전 이미지 버튼 객체 참조
-        ImageButton button_home = findViewById(R.id.home); // 홈 이미지 버튼 객체 참조
+        Button button_previous = findViewById(R.id.previous); //이전 이미지 버튼 객체 참조
+        Button button_home = findViewById(R.id.home); // 홈 이미지 버튼 객체 참조
 
         /*이전 버튼 누를 시 화면 전환*/
         button_previous.setOnClickListener(new View.OnClickListener() {
