c0354ce652e55df1449d5860732e0d3b1ab16fac
zada8 <qqaz0609@naver.com>
2021-11-11 05:22:58 +0900

    stt

--- app/src/main/java/com/android/hearwego/DestinationSearchActivity.java
+++ app/src/main/java/com/android/hearwego/DestinationSearchActivity.java
@@ -1,25 +1,43 @@
 package com.android.hearwego;
 
+import android.Manifest;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class DestinationSearchActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
+
+    //stt
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    Button sttBtn;
+    Button startBtn;
+    TextView textView;
+    final int PERMISSION = 1;
+    //
 
     TMapView tMapView = null;
     TMapGpsManager tMapGpsManager = null;
@@ -51,6 +69,32 @@ public class DestinationSearchActivity extends AppCompatActivity implements TMap
         if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
             uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility( uiOption );
+
+        //stt
+        // 퍼미션 체크
+        if ( Build.VERSION.SDK_INT >= 23 ) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO},PERMISSION);
+        }
+        // xml의 버튼과 텍스트 뷰 연결
+        textView = (TextView)findViewById(R.id.sttResult);
+        sttBtn = (Button) findViewById(R.id.mic_button);
+        startBtn = (Button) findViewById(R.id.search_start_button);
+
+        // RecognizerIntent 객체 생성
+        intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+
+        // 버튼을 클릭 이벤트 - 객체에 Context와 listener를 할당한 후 실행
+        sttBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(DestinationSearchActivity.this);
+                mRecognizer.setRecognitionListener(listener);
+                mRecognizer.startListening(intent);
+            }
+        });
 
         //TextToSpeech 기본 설정
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
@@ -121,6 +165,98 @@ public class DestinationSearchActivity extends AppCompatActivity implements TMap
 
 
     }
+
+    //stt
+    //RecognizerLntent 객체에 할당할 listener 생성
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getApplicationContext(), "음성 인식을 시작합니다.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            //인식 결과가 준비되면 호출
+            //말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            for (int i = 0; i < matches.size(); i++){
+                ((LogoActivity) LogoActivity.context_logo).keyword = matches.get(i);
+                textView.setText(((LogoActivity) LogoActivity.context_logo).keyword);   // 음성 인식한 데이터를 text로 변환해 표시
+            }
+        }
+        @Override
+        public void onBeginningOfSpeech() {}
+
+        @Override
+        public void onRmsChanged(float rmsdB) {}
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {}
+
+        @Override
+        public void onEndOfSpeech() {       // 음성 인식이 제대로 되었을 때의 동작
+            startBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Intent intent = new Intent(DestinationSearchActivity.this, RouteChoiceActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+
+        @Override
+        public void onError(int error) {
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER 가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러가 발생했습니다. : "
+                    + message,Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {}
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {}
+    };
 
     @Override
     public void onLocationChange(Location location) {
