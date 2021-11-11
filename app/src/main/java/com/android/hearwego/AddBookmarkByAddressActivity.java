d47f4981530892d4501f5ca45ffe1cf1866bb0bd
siyunkr <siyunkr@gmail.com>
2021-11-11 23:55:44 +0900

    AddBookmarkByAddressActivity, add_bookmark_byaddress.xml

---
+++ app/src/main/java/com/android/hearwego/AddBookmarkByAddressActivity.java
@@ -0,0 +1,39 @@
+package com.android.hearwego;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddBookmarkByAddressActivity extends AppCompatActivity {
+
+    private View decorView; //full screen 객체 선언
+    private int	uiOption; //full screen 객체 선언
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_bookmark_byaddress);
+
+        ActionBar actionBar = getSupportActionBar(); //액션바(패키지명) 숨김처리
+        actionBar.hide();
+
+        /*전체 화면 모드 -> 소프트 키 없앰*/
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+            uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOption);
+
+        Button button_previous = findViewById(R.id.previous); //이전 이미지 버튼 객체 참조
+        Button button_home = findViewById(R.id.home); // 홈 이미지 버튼 객체 참조
+
+}}
