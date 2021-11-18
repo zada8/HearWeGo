30849458d1d44575bb464be93e2953f7e1a8b78c
goeun9795 <jgn9795@gmail.com>
2021-11-18 22:01:02 +0900

    위도, 경도, 장소명, 키워드명 데이터베이스 등록 수정 (오류 O)

--- app/src/main/java/com/android/hearwego/User.java
+++ app/src/main/java/com/android/hearwego/User.java
@@ -1,19 +1,20 @@
 package com.android.hearwego;
+import com.google.firebase.firestore.GeoPoint;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class User {
 
     public String name;
-    public List<String> keyword;
+    //public Map<String, String> locname;
     public List<String> locname;
+    public List<String> keyword;
     public List<String> latitude;
     public List<String> longtitude;
     public User() {}
     public User(String name){
-        this.name = name;
-    }
-    public User(String name, List<String> keyword, List<String> locname, List<String> latitude, List<String> longtitude ){
         this.name = name;
     }
 }
