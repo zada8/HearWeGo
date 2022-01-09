099eff28fe4613ec716a6db45c9089d60dac4923
zada8 <qqaz0609@naver.com>
2022-01-10 06:35:08 +0900

    도움말tts

--- app/src/main/java/com/android/hearwego/HelpActivity.java
+++ app/src/main/java/com/android/hearwego/HelpActivity.java
@@ -1,24 +1,35 @@
 package com.android.hearwego;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 
 public class HelpActivity extends AppCompatActivity {
 
     private View decorView; //full screen 객체 선언
     private int	uiOption; //full screen 객체 선언
 
+    TextToSpeech textToSpeech;
+
+    int choice = 0; //안내 버튼 선택
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+
 
         ActionBar actionBar = getSupportActionBar(); //액션바(패키지명) 숨김처리
         actionBar.hide();
@@ -34,14 +45,58 @@ public class HelpActivity extends AppCompatActivity {
             uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility( uiOption );
 
+        /*TextToSpeech 기본 설정*/
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.KOREAN);
+                    textToSpeech.setPitch(1.5f);
+                    textToSpeech.setSpeechRate(1.0f);
+                }
+            }
+        });
 
-        Button button_help_navi = findViewById(R.id.help_navi); //공공시설 이미지 버튼 객체 참조
-        Button button_help_surrounding = findViewById(R.id.help_surrounding); //지하철 이미지 버튼 객체 참조
-        Button button_help_bookmark = findViewById(R.id.help_bookmark); //화장실 이미지 버튼 객체 참조
-        Button button_help_setting = findViewById(R.id.help_setting); //병원 이미지 버튼 객체 참조
 
-        Button button_previous = findViewById(R.id.previous); //이전 이미지 버튼 객체 참조
-        Button button_home = findViewById(R.id.home); // 홈 이미지 버튼 객체 참조
+        Button button_help_navi = findViewById(R.id.help_navi); //길안내 버튼 객체 참조
+        Button button_help_surrounding = findViewById(R.id.help_surrounding); //주변시설 버튼 객체 참조
+        Button button_help_bookmark = findViewById(R.id.help_bookmark); //즐겨찾기 버튼 객체 참조
+        Button button_help_setting = findViewById(R.id.help_setting); //설정 버튼 객체 참조
+
+        Button button_previous = findViewById(R.id.previous); //이전 버튼 객체 참조
+        Button button_home = findViewById(R.id.home); // 홈 버튼 객체 참조
+
+        button_help_navi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    choice=R.raw.help_navi;
+            textToSpeech.speak(readTxt(), TextToSpeech.QUEUE_FLUSH, null);
+        }
+        });
+
+        button_help_surrounding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                choice=R.raw.help_surrounding;
+                textToSpeech.speak(readTxt(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        button_help_bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                choice=R.raw.help_bookmark;
+                textToSpeech.speak(readTxt(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        button_help_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                choice=R.raw.help_setting;
+                textToSpeech.speak(readTxt(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
 
 
         //이전 버튼 누를 시 화면 전환
@@ -63,6 +118,38 @@ public class HelpActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private String readTxt() {//txt를 string으로
+        String data = null;
+        @SuppressLint("ResourceType") InputStream inputStream = getResources().openRawResource(choice);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int i;
+        try {
+            i = inputStream.read();
+            while (i != -1) {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+
+            data = new String(byteArrayOutputStream.toByteArray());
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    //tts
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(textToSpeech!=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+            textToSpeech = null;
+        }
     }
 }
 
